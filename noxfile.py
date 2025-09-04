f4fc65ec70537e16d3d152cffe964aaba2a2ea18
Matthias Urlichs <matthias@urlichs.de>
2025-09-04 17:17:25 +0200

    drop requirements* files.

--- noxfile.py
+++ noxfile.py
@@ -8,22 +8,22 @@ nox.options.stop_on_first_error = True
 @nox.session(python=python[-1])
 def test_mypy(session):
     session.install(".")
-    session.install("-rrequirements_test.txt")
-    session.install("-rrequirements_diagrams.txt")
-    session.install("-rrequirements_mypy.txt")
-    session.run("pytest", "-nauto", "--doctest-modules", "tests/")
-
-
-@nox.session(python=python)
-def test(session):
-    session.install(".")
-    session.install("-rrequirements_test.txt")
-    session.install("-rrequirements_diagrams.txt")
-    session.run("pytest", "-nauto", "tests/")
+    session.install(".[dev]")
+    session.install(".[diagrams]")
+    session.install(".[mypy]")
+    session.run("pytest", "-sxv", "--doctest-modules", "tests/")
 
 
 @nox.session(python=python[-1])
+def test(session):
+    session.install(".")
+    session.install(".[dev]")
+    session.install(".[diagrams]")
+    session.run("pytest", "-sxv", "tests/")
+
+
+@nox.session(python=python)
 def test_no_gv(session):
     session.install(".")
-    session.install("-rrequirements_test.txt")
-    session.run("pytest", "-nauto", "tests/")
+    session.install(".[dev]")
+    session.run("pytest", "-sxv", "tests/")
